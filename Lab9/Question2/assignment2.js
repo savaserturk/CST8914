9a71c2be4f5d6e31a85893b8fadeeb7e50970952
Savas Erturk <ertu0002@algonquinlive.com>
2024-11-07 22:57:22 -0500

    fixed errors

--- Lab9/Question2/assignment2.js
+++ Lab9/Question2/assignment2.js
@@ -15,11 +15,14 @@ if (submitBtn) {
 }
 
 function sendComments (e) {
-	//e.preventDefault();  // When uncommented, this will prevent the form from submitting when the button is clicked
+	e.preventDefault();  // When uncommented, this will prevent the form from submitting when the button is clicked
 
 	// This is the message we want added to the aria live region
 	let msg = "Thank you for sending your comments. Your comments have already been thrown into the dustbin of the Interwebs, and will be ignored at once!"	;
 
 	// Now, add the code to insert the msg into the aria live region
+	showMessage.textContent = msg;
+
+	submitBtn.focus();
 	
 } // End of sendComments
